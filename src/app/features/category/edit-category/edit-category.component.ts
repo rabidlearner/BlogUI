07e1d78c95a73750ed4d4b5fe7fd0694e499f794
Tonmoy Mukherjee <tonmoy.105e@gmail.com>
2024-06-26 23:20:37 +0530

    Edit a category

---
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request-model';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrl: './edit-category.component.css'
+})
+export class EditCategoryComponent implements OnInit,OnDestroy{
+  
+  paramsSubscription?: Subscription;
+  editCategorySubscription?: Subscription;
+
+  id: string | null = null;
+  category?: Category;
+
+  constructor(private route: ActivatedRoute,private categoryService: CategoryService,private router: Router) {
+  }
+  
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+
+        if (this.id) {
+          //get data from API for this category id
+          this.categoryService.getCategoryById(this.id).subscribe({
+            next: (response)=>{
+              this.category = response;
+            }
+          });
+        }
+      }
+    });
+  }
+
+  onFormSubmit(): void {
+      const UpdateCategoryRequest: UpdateCategoryRequest = {
+        name: this.category?.name ?? '',
+        urlHandle: this.category?.urlHandle ?? ''
+      };
+
+      //pass this obj to service
+      if(this.id){
+        this.editCategorySubscription = this.categoryService.updateCategory(this.id,UpdateCategoryRequest).subscribe({
+          next: (response) => {
+            this.router.navigateByUrl('/admin/categories');
+          }
+        });
+      }
+      
+    }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.editCategorySubscription?.unsubscribe();
+  }
+
+}
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from '../../../../environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request-model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,11 @@ export class CategoryService {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
+  }
+
+  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest) : Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`, updateCategoryRequest);
+  }
 }
